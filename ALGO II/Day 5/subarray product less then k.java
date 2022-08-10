b21f09bc064ef998fc9deb4599fb6399b7f33b2f
Sunny07S <75514352+Sunny07S@users.noreply.github.com>
2022-08-10 12:47:18 +0530

    Create subarray product less then k.java

---
+++ ALGO II/Day 5/subarray product less then k.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if(k<2)return 0;
+        int left=0,right=0,prod=1,res=0;
+        for(;right<nums.length;right++){
+            prod*=nums[right];
+            while(prod>=k)prod/=nums[left++];
+            res+=right-left+1;
+        }
+        return res;
+    }
+}
